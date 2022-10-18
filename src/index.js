003cfac9c0751114efec83c75958b64929a9ab25
dialloabdourahman <dialliabdourahman78@gmail.com>
2022-10-18 22:42:07 +0100

    Environment variables naming changed

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <Auth0Provider
-    domain={process.env.REACT_APP_AUTH0_DOMAIN}
-    clientId={process.env.REACT_APP_AUTH0_CLIENTID}
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENTID}
     redirectUri={window.location.origin}
     cacheLocation='localstorage'
   >
